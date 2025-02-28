2b25d0c59602390bb3078c47224f4a3093198ce5
JerfessonMelo <jerfessonmelo123@gmail.com>
2025-02-28 16:22:43 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function pegarValor(){
+    let altura = parseInt(document.getElementById("altura").value);
+    let sexoSelecionado = document.querySelector("input[name='sexo']:checked");
+    let pesoIdeal;
+
+    if (sexoSelecionado.value == "Feminino") {
+        pesoIdeal = 52 + (0.67 * (altura / 100));
+        } else if (sexoSelecionado.value == "Masculino") {
+            pesoIdeal = 52 + (0.75 * (altura / 100));
+    }
+
+    document.getElementById("pesoIdeal").textContent = "Peso Ideal: " + pesoIdeal.toFixed(2);
+}
